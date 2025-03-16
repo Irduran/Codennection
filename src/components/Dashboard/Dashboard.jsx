fefdc18ed8864b18dd73d454ced8a72a4005a6dd
Kwang390 <kawinwang390@gmail.com>
2025-03-15 21:25:34 -0600

    comp perfil

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import StarryBackground from '../Star/StarryBackground';
 import './Dashboard.css';
 import TopBar from '../Navigation/TopBar';
+import { ProfileCard } from '../ProfileCard/ProfileCard';
 
 const Dashboard = () => {
   return (
     <>
       <StarryBackground />
       <TopBar/>
+      <ProfileCard/>
       <div className="dashboard-container">
       </div>
     </>
---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './ProfileCard.css'
+export const ProfileCard = () => {
+  return (
+    <>
+    <div className="profile-container d-flex justify-content-center align-items-center">
+      <div className="profile-card p-0">
+        <div className="profile-header text-center">
+          <div className="profile-avatar mx-auto " ></div>
+        </div>
+        <div className="profile-body text-center">
+          <h5 className="profile-username">Username</h5>
+          <p className="profile-bio">BIO</p>
+          <div className="profile-skills d-flex flex-wrap justify-content-center gap-2">
+            <span className="badge">React</span>
+            <span className="badge">Javascript</span>
+            <span className="badge">Python</span>
+            <span className="badge">Node.js</span>
+          </div>
+        </div>
+        <div className="profile-footer text-center">
+          <button className="profile-button">My Profile</button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
